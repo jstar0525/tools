351209da6a94f7eac09debf931ca2f5ff9ad96fd
Jstar0525 <jstar0525@gmail.com>
2021-03-05 15:02:07 +0900

    revise labelme file for json2dataset.py

---
+++ labelme/README.md
@@ -0,0 +1,23 @@
+# Reference
+https://github.com/wkentaro/labelme
+
+
+# Annotation
+```
+# change dir to label dataset
+cd tools/labelme/data/labeled
+
+# excute labelme for annotation
+labelme ./ --labels label_names.txt
+```
+
+# json2dataset
+
+make multi json file to dataset
+
+```
+* json dir    :   ./tools/labelme/data/labeled
+* dataset dir :   ./tools/labelme/data/dataset
+```
+
+![explain img](./doc/json2dataset.png)
--- labelme/json2dataset.py
+++ labelme/json2dataset.py
@@ -1,59 +1,78 @@
-import base64
+
+# ref https://github.com/wkentaro/labelme/blob/master/labelme/cli/json_to_dataset.py
+
 import json
 import os
 import os.path as osp
 
 import imgviz
 import PIL.Image
+import numpy as np
 
-import utils
+from labelme import utils
 
-DATASET_PATH = './data/'
-JSON_FOLDER = 'json/'
-SAVE_DIR = DATASET_PATH + 'dataset/'        
-_, _, jsons = next(os.walk(os.path.join(DATASET_PATH, JSON_FOLDER)))
+JSON_DIR = './labelme/data/labeled/'
+SAVE_DIR = './labelme/data/dataset/'
 
-for json_file in jsons:
+def main(JSON_DIR, SAVE_DIR):
 
-    data = json.load(open(DATASET_PATH + JSON_FOLDER + json_file))
-    imageData = data.get("imageData")
+    # read .json file list
+    _, _, jsons = next(os.walk(JSON_DIR))
+    jsons = [s for s in jsons if ".json" in s]
     
-    if not imageData:
-        imagePath = os.path.join(os.path.dirname(json_file), data["imagePath"])
-        with open(imagePath, "rb") as f:
-            imageData = f.read()
-            imageData = base64.b64encode(imageData).decode("utf-8")
-    img = utils.img_b64_to_arr(imageData)
+    # take the label_names.txt
+    with open(osp.join(JSON_DIR, "label_names.txt"), "r") as f:
+        cnt = 0
+        label_name_to_value = {}
+        for line in f:
+            label_name_to_value[line.rstrip('\n')] = cnt
+            cnt += 1
     
-    label_name_to_value = {"_background_": 0, "joint": 1, "stem": 2, "growth_point": 3}
-    # label_name_to_value = {"_background_": 0}
-    # for shape in sorted(data["shapes"], key=lambda x: x["label"]):
-    #     label_name = shape["label"]
-    #     if label_name in label_name_to_value:
-    #         label_value = label_name_to_value[label_name]
-    #     else:
-    #         label_value = len(label_name_to_value)
-    #         label_name_to_value[label_name] = label_value
-    lbl, _ = utils.shapes_to_label(
-        img.shape, data["shapes"], label_name_to_value
-    )
+    for json_file in jsons:
+        
+        # read json
+        data = json.load(open(JSON_DIR + json_file))
+        
+        # read image
+        imageData = data.get("imageData")
+        
+        if not imageData:
+            imagePath = os.path.join(JSON_DIR, data["imagePath"])
+            img = np.asarray(PIL.Image.open(imagePath))
+        else:
+            img = utils.img_b64_to_arr(imageData)
+        
+        with open(osp.join(JSON_DIR, "label_names.txt"), "r") as f:
+            cnt = 0
+            label_name_to_value = {}
+            for line in f:
+                label_name_to_value[line.rstrip('\n')] = cnt
+                cnt += 1
+        
+        # make a label data
+        lbl, _ = utils.shapes_to_label(
+            img.shape, data["shapes"], label_name_to_value
+        )
+        
+        # make a viz data
+        label_names = [None] * (max(label_name_to_value.values()) + 1)
+        for name, value in label_name_to_value.items():
+            label_names[value] = name
+        
+        lbl_viz = imgviz.label2rgb(
+            label=lbl, img=imgviz.asgray(img), label_names=label_names, loc="rb"
+        )
+        
+        # save dataset
+        _, name, _ = json_file.replace('.', '_').split('_')
+        
+        PIL.Image.fromarray(img).save(osp.join(SAVE_DIR, "img_" + name + ".png"))
+        utils.lblsave(osp.join(SAVE_DIR, "label_" + name + ".png"), lbl)
+        PIL.Image.fromarray(lbl_viz).save(osp.join(SAVE_DIR, "viz_" + name + ".png"))
+        
+        with open(osp.join(SAVE_DIR, "label_names.txt"), "w") as f:
+            for lbl_name in label_names:
+                f.write(lbl_name + "\n")
     
-    label_names = [None] * (max(label_name_to_value.values()) + 1)
-    for name, value in label_name_to_value.items():
-        label_names[value] = name
-    
-    lbl_viz = imgviz.label2rgb(
-        label=lbl, img=imgviz.asgray(img), label_names=label_names, loc="rb"
-    )
-    
-    _, name, _ = json_file.replace('.', '_').split('_')
-    
-    PIL.Image.fromarray(img).save(osp.join(SAVE_DIR, "img_" + name + ".png"))
-    utils.lblsave(osp.join(SAVE_DIR, "label_" + name + ".png"), lbl)
-    PIL.Image.fromarray(lbl_viz).save(osp.join(SAVE_DIR, "viz_" + name + ".png"))
-    
-    with open(osp.join(SAVE_DIR, "label_names.txt"), "w") as f:
-        for lbl_name in label_names:
-            f.write(lbl_name + "\n")
-    
-
+if __name__ == "__main__":
+	main(JSON_DIR, SAVE_DIR)
